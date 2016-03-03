1c4be20ec210a87737da1876eb2a094b8bb3f038
ravenNNN <baltazarparra@outlook.com>
2016-03-03 05:57:43 -0300

    exercicio js

--- 07 - Arrays/7.1-criando-arrays.js
+++ 07 - Arrays/7.1-criando-arrays.js
@@ -1,3 +1,13 @@
 /**
  * Created by baltazar on 02/03/16.
  */
+
+//	Exercicio
+
+var ola = ['ola mundo'];
+var hello = ola.toString();
+var cut = hello.split('');
+var final = cut.reverse();
+var console.log(final);
+
+//-------------------//
