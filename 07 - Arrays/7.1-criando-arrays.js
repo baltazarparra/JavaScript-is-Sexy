6a21d49f84aacc909682e6dffad8fb982574b8b2
ravenNNN <baltazarparra@outlook.com>
2016-03-03 06:14:18 -0300

    exercicio js

--- 07 - Arrays/7.1-criando-arrays.js
+++ 07 - Arrays/7.1-criando-arrays.js
@@ -2,12 +2,38 @@
  * Created by baltazar on 02/03/16.
  */
 
+// CRIANDO
+
+var empty = [];
+
+// LENDO E GRAVANDO
+
+var a = ['world'];
+var value = a[0];
+
+// ADICIONAR E EXCLUIR
+
+a = [];
+a[0] = 'zero';
+a[1] = 'one';
+console.log(a);
+
+a.push('three');
+
+a.push('four', 'five');
+
+console.log(a);
+
+delete a[0];
+
+console.log(a);
+
 //	Exercicio
 
 var ola = ['ola mundo'];
 var hello = ola.toString();
 var cut = hello.split('');
 var final = cut.reverse();
-var console.log(final);
+console.log(final);
 
 //-------------------//
