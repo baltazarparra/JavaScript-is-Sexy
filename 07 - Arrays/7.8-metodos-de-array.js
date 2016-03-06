de2d4a9b7c2dfe86a3a4fdff5332b92945a55e53
ravenNNN <baltazarparra@outlook.com>
2016-03-06 09:25:12 -0300

    Add metodos de array.

--- 07 - Arrays/7.8-metodos-de-array.js
+++ 07 - Arrays/7.8-metodos-de-array.js
@@ -1,3 +1,47 @@
 /**
  * Created by baltazar on 06/03/16.
  */
+
+// JOIN
+
+var a = [1, 2, 3];
+a.join();
+console.log(a);
+var b = a.join(' ');
+console.log(b);
+var c = a.join('');
+console.log(c);
+var d = new Array(10);
+var e = d.join('-');
+console.log(d);
+console.log(e);
+
+// REVERSE
+
+var r = [1, 2, 3];
+var f = r.reverse();
+console.log(f);
+var g = r.reverse().join();
+console.log(g);
+
+// SORT
+
+var s = ['banana', 'cherry', 'apple'];
+var h = s.sort();
+console.log(h);
+var j = h.join(', ');
+console.log(j);
+
+//--------------//
+
+var n = [33, 4, 1111, 222];
+n.sort();
+n.sort(function(a, b) {
+	return a-b;
+});
+console.log(n);
+var m = n.sort(function(a, b){return b-a;});
+console.log(m);
+
+//--------------//
+
