78f476827ddc447d7c41ad5e80f38d04eb80c65c
ravenNNN <baltazarparra@outlook.com>
2016-03-07 06:48:43 -0300

    Fim do 7.9

--- 07 - Arrays/7.9-metodos-array-ecma5.js
+++ 07 - Arrays/7.9-metodos-array-ecma5.js
@@ -30,3 +30,45 @@ p = m.map(function(x) {return x*x; });
 console.log(p);
 
 // FILTER
+
+f = [5, 4, 3, 2, 1];
+smallvalues = f.filter(function(x) { return x < 3 });
+console.log(smallvalues);
+everyother = f.filter(function(x, i) { return i%2==0 });
+console.log(everyother);
+
+// EVERY E SOME
+
+e = [1, 2, 3, 4, 6];
+var s = e.every(function(x) {return x < 10;});
+console.log(s);
+var fal = e.every(function(x) {return x % 2 === 0;});
+console.log(fal);
+
+//-----------------//
+
+som = [1, 2, 3, 4, 5];
+som.some(function(x) {return x%2===0; });
+
+// REDUCE
+
+function extend(o, p) {
+	for(prop in p) {
+		o[prop] = p[prop];
+	}
+	return o;
+}
+
+function union(o,p) { return extend(extend({},o), p); }
+
+var objects = [{x:1}, {y:2}, {z:3}];
+var merged = objects.reduce(union);
+console.log(objects);
+console.log(merged);
+
+// INDEXOF
+
+a = [0, 1, 2, 1, 0];
+console.log(a.indexOf(1));
+console.log(a.lastIndexOf(1));
+console.log(a.indexOf(3));
