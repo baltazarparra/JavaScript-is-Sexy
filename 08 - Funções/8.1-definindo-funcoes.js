11916142b29302bdb45e5634cdb0cb27c610971d
ravenNNN <baltazarparra@outlook.com>
2016-03-08 06:51:54 -0300

    Exemplo 8-1

--- 08 - Funções/8.1-definindo-funcoes.js
+++ 08 - Funções/8.1-definindo-funcoes.js
@@ -1,3 +1,24 @@
 /**
  * Created by baltazar on 08/03/16.
  */
+
+// FUNCOES ANINHADAS
+
+function hypotenuse (a, b) {
+	function square(x) {return x*x; }
+	return Math.sqrt(square(a) + square(b));
+}
+
+console.log(hypotenuse(8, 8));
+
+// CHAMADA DE METODO
+
+var calculator = {
+	ope1: 1,
+	ope2: 1,
+	add: function() {
+		this.result = this.ope1 + this.ope2;
+	}
+};
+calculator.add();
+console.log(calculator.result);
