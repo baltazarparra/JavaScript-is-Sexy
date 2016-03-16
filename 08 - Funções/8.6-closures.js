15616b19a85cf1b0c739bba1da58965f361f8d7d
ravenNNN <baltazarparra@outlook.com>
2016-03-16 05:37:44 -0300

    8.6 - Closures

--- 08 - Funções/8.6-closures.js
+++ 08 - Funções/8.6-closures.js
@@ -13,3 +13,39 @@ var testscope = checkscope()();
 
 console.log(testscope);
 console.log(scope);
+
+//----------------------//
+
+var uniqueInterger = (function() {
+	var counter = 0;
+	return function() {return counter++;};
+}());
+
+uniqueInterger();
+uniqueInterger();
+console.log(uniqueInterger());
+
+//-----------------------------//
+
+function counter() {
+	var n = 0;
+	return {
+		count: function() {return n++;},
+		reset: function() {n = 0;}
+	};
+}
+
+var c = counter(), d = counter();
+var ce = c.count();
+console.log(ce);
+var de = d.count();
+console.log(de);
+var ce = c.reset();
+console.log(ce);
+var ce = c.count();
+console.log(ce);
+var de = d.count();
+console.log(de);
+
+//---------------------//
+
